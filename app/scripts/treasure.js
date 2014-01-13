229f127d4f33eb47c559e49791bcda85ce14efcf
John Gallagher <john@synapticmishap.co.uk>
2014-01-13 00:07:55 +0000

    We now add search choices to the list.

--- app/scripts/treasure.js
+++ app/scripts/treasure.js
@@ -4,38 +4,102 @@ var Choice = Backbone.Model.extend({
 
 var Choices = Backbone.Collection.extend({
   model: Choice,
-  url: "http://jgmacbookpro.local:3000/api/v1/terms"
+  termList: function() {
+    return this.models.map(function(choice) {
+      return choice.get('id');
+    });
+  }
+});
+var choices = new Choices;
+
+var Dropdown = Backbone.Collection.extend({
+  model: Choice,
+  url: "http://jgmacbookpro.local:3000/api/v1/terms",
+  initialSearch: function() {
+    this.search('');
+    $('input#search').focus();
+  },
+  search: function(term) {
+    this.fetch({
+      data: { 
+        q: term,
+        terms: choices.termList()
+      }
+      , reset: true
+    })
+  }
 });
 
-ChoicesView = Backbone.View.extend({
-  el: '#choices',
+var dropdown = new Dropdown;
+
+DropdownView = Backbone.View.extend({
+  el: '#dropdown',
+  events: {
+    'click': 'selectChoice'
+  },
 
   initialize: function() {
-    this.template = _.template($('#choices-template').html());
+    this.template = _.template($('#drop-template').html());
     this.collection.bind("reset", this.render, this);
   },
 
+  selectChoice: function(el) {
+    choice = dropdown.findWhere({name: $(el.target).data('name'), text: $(el.target).data('text')});
+    dropdown.remove(choice);
+    choices.add(choice);
+    this.render();
+    dropdown.initialSearch();
+  },
+
   render: function() {
-    console.log("just rendered");
+    console.log("just rendered dropdown");
     var renderedContent = this.template(this.collection);
     $(this.el).html(renderedContent);
     return this;
   }
 });
 
-$(document).ready(function () {
-  var choices = new Choices;
-  var view = new ChoicesView({ collection: choices });
+var dropdownView = new DropdownView({ collection: dropdown });
 
-  $('input#search').keyup(function() {
-    choices.fetch({ 
-      data: { 
-        q: $('input#search').val(),
-        terms: []
-      }
-      , reset: true
-    })
-  })
+ChoicesView = Backbone.View.extend({
+  el: '#choices',
+  events: {
+    'keyup': 'search'
+  },
+
+  initialize: function() {
+    this.template = _.template($('#choices-template').html());
+    this.collection.bind("reset", this.render, this);
+    this.collection.bind("add", this.render, this);
+  },
+
+  search: function(el) {
+    dropdown.search($('input#search').val());
+  },
+
+  render: function() {
+    console.log("just rendered choices");
+    var renderedContent = this.template(this.collection);
+    $(this.el).html(renderedContent);
+    return this;
+  }
+});
+
+var choicesView = new ChoicesView({ collection: choices });
+
+$(document).ready(function () {
+  dropdown.reset();
+  choices.reset();
+
+  // $('input#search').keyup(function() {
+  //   dropdown.fetch({ 
+  //     data: { 
+  //       q: $('input#search').val(),
+  //       terms: []
+  //     }
+  //     , reset: true
+  //   });
+  // });
 
   // $('input#search').on("focus", function(e) {
   //   $(this).hide();
@@ -44,8 +108,4 @@ $(document).ready(function () {
   //     $(this).show();
   //   }.bind(this));
   // });
-
-
-
-
 });
